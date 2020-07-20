be15bbb2cc7e270c9b8b3dbda2f9c2b81d314e51
unknown <humesh.sindpure@9abusiness.com>
2020-07-20 12:13:32 +0530

    Updates

---
+++ precache-manifest.4e97f1774b549033cd59b0490df8b802.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86aa983f7daa969c18e78a423f7ea0e",
+    "url": "/mystudymaterial/index.html"
+  },
+  {
+    "revision": "1f39b823f1ef6f8b973f",
+    "url": "/mystudymaterial/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "db1c2ce748ee1c00d16f",
+    "url": "/mystudymaterial/static/css/main.9fb135f9.chunk.css"
+  },
+  {
+    "revision": "1f39b823f1ef6f8b973f",
+    "url": "/mystudymaterial/static/js/2.029046d0.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/mystudymaterial/static/js/2.029046d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db1c2ce748ee1c00d16f",
+    "url": "/mystudymaterial/static/js/main.681a482e.chunk.js"
+  },
+  {
+    "revision": "d3bf20a57317f0341475",
+    "url": "/mystudymaterial/static/js/runtime-main.94669ede.js"
+  },
+  {
+    "revision": "645f061822725d4256235a69ba5769aa",
+    "url": "/mystudymaterial/static/media/android.645f0618.jpg"
+  },
+  {
+    "revision": "a3a8ede79a2153a5cdaf37d4b1de807c",
+    "url": "/mystudymaterial/static/media/code.a3a8ede7.jpg"
+  },
+  {
+    "revision": "a8f3db075ebc6b7d1e86718154729da4",
+    "url": "/mystudymaterial/static/media/dashboard.a8f3db07.jpg"
+  },
+  {
+    "revision": "5ecf1ab8ee8b2bd972c3abca71390e43",
+    "url": "/mystudymaterial/static/media/python.5ecf1ab8.jpg"
+  },
+  {
+    "revision": "9474df2e1107299b495a0abf26654770",
+    "url": "/mystudymaterial/static/media/study.9474df2e.jpg"
+  },
+  {
+    "revision": "91a06d8a149fa484d1814577b298cda0",
+    "url": "/mystudymaterial/static/media/web.91a06d8a.svg"
+  }
+]);
